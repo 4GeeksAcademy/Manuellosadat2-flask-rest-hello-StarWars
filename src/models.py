a744a6c07f117cec7bb36bb9b093d4328d7897ab
manuellosadat2-a11y <manuellosadat2@gmail.com>
2026-03-04 21:13:47 +0000

    flask rest

--- src/models.py
+++ src/models.py
@@ -78,7 +78,7 @@ class Favorite(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
-    user_id = db.Column(db.Integer, db.ForeignKEey("user.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"))
     character_id = db.Column(db.Integer, db.ForeignKey("character.id"))
 
